5961e7dc667b8d57073e462e6a25cf82750cc005
Toomun <124077335+Toomun@users.noreply.github.com>
2023-02-28 21:04:37 -0300

    proyectocv

---
+++ JS/function.js
@@ -0,0 +1,5 @@
+
+document.getElementById('botón').onclick = function () {
+    console.log("Capturamos el evento Click");
+    document.getElementById("demo").innerHTML ="Trabajo en equipo, flexibilidad, creatividad, liderazgo y buena comunicación.";
+}
